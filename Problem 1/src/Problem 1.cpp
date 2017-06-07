a194c98311720e73764d62cc21d2e2111683a702
Abdullah Khan <cmabdullah21@gmail.com>
2017-06-07 21:53:59 +0600

    Done problem 1

--- Problem 1/src/Problem 1.cpp
+++ Problem 1/src/Problem 1.cpp
@@ -5,11 +5,25 @@
 // Copyright   : OpenSource
 // Description : Hello World in C++, Ansi-style
 //============================================================================
-
+/***
+ * You will be given an array of integers,
+ * write a recursive solution to print it in reverse order.
+ * Input: 5 69 87 45 21 47 Output: 47 21 45 87 69
+ * */
 #include <iostream>
 using namespace std;
-
+void reverse  (int i);
 int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	reverse(5);
 	return 0;
+}
+
+void reverse (int i){
+	int arr[] = {1,2,3,4,5,7};
+	printf("index[%d] %d \n",i, arr[i]);
+	if (i == 0)
+	return   ;
+	else
+		reverse (i-1 );
 }

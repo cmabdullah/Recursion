0f1d05706bf041af4002c27ab915dec81e63c067
C M Abdullah Khan <a.kium.khan@gmail.com>
2017-06-10 22:18:44 -0700

    problem 2 solved

--- Problem 2/src/Problem 2.cpp
+++ Problem 2/src/Problem 2.cpp
@@ -7,9 +7,28 @@
 //============================================================================
 
 #include <iostream>
-using namespace std;
-
-int main() {
-	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
-	return 0;
+ using namespace std;
+/**************
+algorithm
+[0] [n-1]
+[1] [n-2]
+.
+.
+.
+.
+.
+[(n-1)/2] [n/2]
+***/
+ void recurs(int i, int j);
+ int main(){
+    int n = 10;
+    recurs(0, n-1);
+    return 0;
+ }
+void recurs(int i, int j){
+    int a[] = {2,4,5,8,10,12,13,14,15,16};
+    if(i<=j){
+    printf("%d\t %d\n", a[i],a[j]);
+    recurs(i+1, j-1);
+    }
 }

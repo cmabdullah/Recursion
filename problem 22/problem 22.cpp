199474f9fbc3bd8de9156a1cf9cea5c2603bbfe0
cmabdullah <cmabdullah21@gmail.com>
2017-06-19 23:59:03 +0600

    Done binom

---
+++ problem 22/problem 22.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : problem.cpp
+// Author      : C M Abdullah Khan
+// Version     :
+// Copyright   : OpenSource
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "Problem 19" << endl; // prints Problem 19
+	return 0;
+}
